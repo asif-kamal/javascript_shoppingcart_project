6a04238e2de5287f3324e4b5e7a2cd23887050ff
asif-kamal <asif.kamal1489@gmail.com>
2020-12-05 20:21:24 -0500

    Created new JS file for Item.

---
+++ javascript-shoppingcart-frontend/src/components/Item.js
@@ -0,0 +1,43 @@
+class Item {
+
+    static main = document.querySelector('main')
+
+    constructor(item){
+        this.name = item.name;
+        this.price = item.price;
+        this.image = item.image;
+        this.quantity = item.quantity;
+        this.category_id = item.category_id
+        this.item = item;
+        this.renderItemCard();
+       
+        
+    }
+
+    static getAll() {
+        api.getAllItems().then((data) =>
+        data.forEach((item) => new Item(item)))
+    }
+
+    itemCard() {
+
+        return `<div class ="card">
+        <div class ="header" data-id="${this.id}"> <p>${this.name}</p>
+        <img src="${this.image}" height="150" width="200" alt="" >
+        <p class="price">${this.price}</p>
+        <p class="quantity">${this.quantity}</p>
+        <div class="btn-group" role="group" aria-label="Basic example">
+        <button type="button" id="${this.id}" class="add btn btn-secondary">Add to Cart</button>
+        
+                  </div>`
+    
+    }
+
+    renderItemCard() {
+    
+    this.constructor.main.innerHTML += this.itemCard()
+}
+
+ 
+
+}
